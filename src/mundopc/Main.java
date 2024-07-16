53061a740400b431450c2b12f9e1e48b41661ba1
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-16 17:14:55 -0500

    feat: Create Monitor class

--- src/mundopc/Main.java
+++ src/mundopc/Main.java
@@ -1,7 +1,19 @@
 package mundopc;
 
+import mundopc.modelo.Monitor;
+import mundopc.modelo.Raton;
+import mundopc.modelo.Teclado;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Raton ratonLenovo = new Raton("bluetooth", "lenovo");
+        System.out.println(ratonLenovo);
+
+        Teclado tecladoLenovo = new Teclado("bluetooth", "lenovo");
+        System.out.println(tecladoLenovo);
+
+        Monitor monitorLenovo = new Monitor("Lenovo", 27);
+        System.out.println(monitorLenovo);
     }
+
 }
